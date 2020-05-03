819504a5bf6f4ac88d17b4c47c250aec2818f5b8
GeorgeGotin <56473382+GeorgeGotin@users.noreply.github.com>
2020-05-03 22:55:14 +0300

    Add files via upload

--- Derivate.py
+++ Derivate.py
@@ -1,14 +1,39 @@
+import math as m
+
 def derivative(f,x,h):
 	return (f(x+h)-f(x))/h
 
 
-def f(x):
+def f1(x):
 	return x**5+17*x**2
-def f_d(x):
+def f1_d(x):
 	return 5*x**4+34*x
+	
+def f2(x):
+	return m.sin(x)*m.cos(x)
+
+def f2_d(x):
+	return (m.cos(x))**2-(m.sin(x))**2
+
+def f3(x):
+	return x+m.sin(x)+m.exp(x)
+
+def f3_d(x):
+	return 1+m.cos(x)+m.exp(x)
 
 import matplotlib.pyplot as plt
 
 
-plt.plot([i for i in range(10)],[f_d(0)-derivative(f,0,0.1**i) for i in range(10)])
+fs=[[f1,f1_d,'x^5+17*x^2'],[f2,f2_d,'sin(x)*cos(x)'],[f3,f3_d,'x+sin(x)+e^x']]
+fig = plt.figure()
+a=[]
+for i in range(3):
+	a.append(fig.add_subplot(1,3,i+1))
+	x = [j for j in range(10)]
+	y = [fs[i][1](0)-derivative(fs[i][0],0,0.1**j) for j in range(10)]
+	a[i].grid(axis='both')
+	a[i].set_title(fs[i][2])
+	a[i].plot(x,y)
+
 plt.show()
+
